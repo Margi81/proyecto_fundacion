b4ebe1f98a493432701aad0bc465bb70f149dd05
Margarita Alonso <margara.8121@gmail.com>
2023-10-18 23:37:12 -0500

    creacion_carpetas_paginasypublic/modificaciones_en_todos_los_archivos

---
+++ public/js/script.js
@@ -0,0 +1,95 @@
+document.addEventListener("DOMContentLoaded", function() {
+  // Datos de los perritos (nombre, imagen, descripción, etc.)
+  const dogsData = [
+      {
+          imagen: '/imagen/mascotas/Mascota_bluey.jpg',
+          nombre: 'Bluey',
+          descripcion: 'Perrita aproximadamente de 2 años'
+      },
+      {
+          imagen: '/imagen/mascotas/Mascota_roko.jpg',
+          nombre: 'Roko',
+          descripcion: 'Perrito de 6 años'
+      },
+      {
+          imagen: '/imagen/mascotas/Mascota_jiji.jpg',
+          nombre: 'Jiji',
+          descripcion: 'Gata aproximadamente de 2 años'
+      },
+      {
+          imagen: '/imagen/mascotas/Mascota_ulises.jpg',
+          nombre: 'Ulises',
+          descripcion: 'Perro con edad aproximada de 3 años'
+      }
+  ];
+
+  const catalog = document.getElementById("catalog");
+
+  dogsData.forEach((dog) => {
+      const contenedorMascota = document.createElement("div");
+      contenedorMascota.classList.add("mascota-contenedor");
+
+      const dogImage = document.createElement("img");
+      dogImage.src = dog.imagen;
+      dogImage.alt = dog.nombre;
+
+      const dogName = document.createElement("p");
+      dogName.textContent = dog.nombre;
+
+      const dogDescription = document.createElement("p");
+      dogDescription.textContent = dog.descripcion;
+
+      contenedorMascota.appendChild(dogImage);
+      contenedorMascota.appendChild(dogName);
+      contenedorMascota.appendChild(dogDescription);
+      catalog.appendChild(contenedorMascota);
+
+      // Agregar un event listener para manejar el clic en los elementos del catálogo
+      contenedorMascota.addEventListener("click", function () {
+          // Tu código para manejar el clic aquí
+          // Puedes abrir una página de detalles del perrito o realizar otras acciones.
+      });
+  });
+});
+
+
+
+ //Seccion formularios Adopcion
+ document.addEventListener("DOMContentLoaded", function () {
+  const formularioAdopcion = document.getElementById("formularioAdopcion");
+  const mostrarFormularioAdopcion = document.getElementById("mostrarFormularioAdopcion");
+
+  mostrarFormularioAdopcion.addEventListener("click", function () {
+      // Cambia la clase del formulario para mostrarlo u ocultarlo
+      formularioAdopcion.classList.toggle("oculto");
+  });
+});
+
+
+//Seccion servicios
+function toggleInfo(servicio) {
+  const infoElement = document.getElementById(`${servicio}-info`);
+
+  if (infoElement.classList.contains('hidden')) {
+    const allInfoElements = document.querySelectorAll('.info');
+    allInfoElements.forEach((element) => element.classList.add('hidden'));
+
+    infoElement.classList.remove('hidden');
+    infoElement.classList.add('visible');
+  } else {
+    infoElement.classList.remove('visible');
+    infoElement.classList.add('hidden');
+  }
+}
+
+// Asignar la función a los elementos de servicio
+const servicios = document.querySelectorAll('.servicio');
+servicios.forEach((servicio) => {
+  servicio.addEventListener('click', (event) => {
+    event.stopPropagation(); // Evitar que el clic se propague y oculte la información
+    const servicioId = servicio.getAttribute('id');
+    toggleInfo(servicioId);
+  });
+});
+
+  
--- server.js
+++ server.js
@@ -1,9 +1,26 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
-
-app.use(express.static(__dirname));  // Sirve los archivos estáticos del directorio actual
-
 const PORT = process.env.PORT || 3000;
+
+// Configurar el middleware para servir archivos estáticos desde "public"
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Configurar el middleware para servir archivos estáticos desde "imagen"
+app.use('/imagen', express.static(path.join(__dirname, 'imagen')));
+
+// Configurar el middleware para servir páginas desde "paginas"
+app.use('/paginas', express.static(path.join(__dirname, 'paginas')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/sobre-nosotros', (req, res) => {
+  res.sendFile(path.join(__dirname, 'paginas', 'sobreNosotros.html'));
+});
+
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}/`);
 });
